62f8b7a3400afb23197feb9f353485f51c08b98c
Laura Fernandes <lauraifernandes2@gmail.com>
2022-05-20 11:15:06 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,55 @@
+//---------------------------------------------------------------------- JS PÁGINA LOGIN -------------------------------------------------------
+
+// -----> VALIDAÇÃO DO LOGIN <-----
+let login = window.sessionStorage.getItem("login");
+
+if (login) {
+  window.location = "./home.html";
+}
+
+// -----> DECLARAÇÃO DAS VARIÁVEIS <-----
+
+let emailInput = document.getElementById("emaillogin");
+let senhaInput = document.getElementById("senhalogin");
+let formLogin = document.querySelector("#formlogin");
+
+// -----> EVENTOS <-----
+
+formLogin.addEventListener("submit", verificaUsuario);
+
+// -----> FUNÇÕES <-----
+
+function verificaUsuario(e) {
+  e.preventDefault();
+  let usuarios = buscaListaUser();
+  let email = emailInput.value;
+  let senha = senhaInput.value;
+
+  let validaEmail = usuarios.some(
+    (elemento) => elemento.emailUser === email );
+  let validaSenha = usuarios.some(
+    (elemento) => elemento.senhaUser === senha
+  );
+
+  if(validaEmail){
+
+    if(validaSenha){
+
+    let posicao = usuarios.findIndex((elemento) => elemento.emailUser === email);
+
+    window.sessionStorage.setItem("login", true);
+    window.sessionStorage.setItem("indexUser", posicao);
+    window.location = "./home.html";
+
+    }else{
+
+      alert("Senha incorreta")
+    }
+  }else{
+    alert("Não existe conta com este e-mail")
+  }
+}
+
+function buscaListaUser() {
+  return JSON.parse(localStorage.getItem("usuario")) || [];
+}
